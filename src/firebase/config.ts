4b0a1f00981f7f959947bf92bfb98be49f7e07b1
تطبيق١ تطبيق <appflutterconnet@gmail.com>
2026-03-19 03:29:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1728867102-529f0",
+  "appId": "1:641727834178:web:d842ed50d680aa7b8725db",
+  "apiKey": "AIzaSyAWJVfwb2uSSV5MZjcNQpJ74bb2CQbCa2k",
+  "authDomain": "studio-1728867102-529f0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "641727834178"
+};
